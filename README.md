377e5483d2325fc42d3b8c3a7746f8be1b8e014f
errorsolver <72688274+errorsolver@users.noreply.github.com>
2023-05-09 16:23:52 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tutorial-Your-first-Angular-app
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/kncwtp-78dgjq)
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+  <section>
+    <form (submit)="filterResults($event, filter.value)">
+      <input type="text" placeholder="Filter by city" #filter>
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+  <section class="results">
+  <app-housing-location
+    *ngFor="let housingLocation of filteredLocationList"
+    [housingLocation]="housingLocation">
+  </app-housing-location>
+  </section>
+  `,
+  styleUrls: ['./home.component.css'],
+})
+
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  filteredLocationList: HousingLocation[] = [];
+
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+
+  filterResults(event: any, text: string) {
+    event.preventDefault(); //menhindari website ter-refresh
+
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
